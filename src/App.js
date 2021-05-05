1f8e526477a0fd4d4913340605c8fcc82d683354
Jeremy Wood <js.wood206@gmail.com>
2021-05-04 21:07:27 -0700

    commit tree 1ea561b6dd4aafec4daeab52dfa1f428e1ad7248

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
-// create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+    constructor() {
+        super()
+        this.state ={
+            message: 'FAIL'
+        }
+    }
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp => resp.json())
+        .then(data => this.updateMessage(data.message))
+    }
+
+    updateMessage = msg => {
+        this.setState({
+            message: msg
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.message}
+            </div>
+        )
+    }
+}
